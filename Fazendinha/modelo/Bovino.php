19d3d890856e558cad8f8d69288be02cfa496aa1
Guilherme Canever Wernke <134728943+GuilhermeWernke@users.noreply.github.com>
2024-11-14 15:36:10 -0300

    Add files via upload

---
+++ Fazendinha/modelo/Bovino.php
@@ -0,0 +1,12 @@
+<?php
+    
+    require_once ('Animal.php');
+    
+    class Bovino extends Animal
+    {
+        
+        
+        
+    }
+    
+?>
---
+++ Fazendinha/modelo/Equineo.php
@@ -0,0 +1,12 @@
+<?php
+    
+    require_once ('Animal.php');
+    
+    class Equineo extends Animal
+    {
+        
+        
+        
+    }
+    
+?>
